b09e918209acebfc838e1cd560660721cce6068a
Alex Vasyaev <alexwoodyside@gmail.com>
2020-11-13 23:28:28 +0300

    init

--- src/main/java/ru/alex/webapp/service/ProductService.java
+++ src/main/java/ru/alex/webapp/service/ProductService.java
@@ -25,7 +25,7 @@ public class ProductService {
     }
 
     public List<Product> findAll() {
-        log.info("In ProductService find all");
+        log.info("In ProductService findall");
         return productRepository.findAll();
     }
 
